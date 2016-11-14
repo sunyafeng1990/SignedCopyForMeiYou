6de17572a680c3764a938ca438a1d92f61d2f210
孙亚锋 <sun@leimo22.local>
2016-11-14 09:56:51 +0800

    Initial Commit

---
+++ SignedTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SignedTest
+//
+//  Created by 孙亚锋 on 16/11/14.
+//  Copyright © 2016年 孙亚锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SignedTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SignedTest
+//
+//  Created by 孙亚锋 on 16/11/14.
+//  Copyright © 2016年 孙亚锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
